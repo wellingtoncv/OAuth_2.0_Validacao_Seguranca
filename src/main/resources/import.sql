7d32a7659b897e5ba7249136e7e51eb97906c8f4
Wellingtoncvs <wellington.santos@addasoftwares.com>
2024-06-01 20:18:31 -0300

    BCrypt password

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$wS4JVMgspNug.jiitUABYuMxoSTxByGPvUij9C7DK9D2t9h4zwpNa');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$wS4JVMgspNug.jiitUABYuMxoSTxByGPvUij9C7DK9D2t9h4zwpNa');
 
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
